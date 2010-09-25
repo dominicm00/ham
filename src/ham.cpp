2652e2f00b3d08ff538ab71fa9050b957ad83874
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-09-25 11:12:07 +0200

    Explicitly qualify the stuff from code:: instead of "using" it.

--- src/ham.cpp
+++ src/ham.cpp
@@ -32,45 +32,6 @@
 #include "code/While.h"
 
 
-// TODO: Remove!
-using code::DumpContext;
-using code::EvaluationContext;
-using code::List;
-using code::ListList;
-using code::Node;
-using code::EqualExpression;
-using code::NotEqualExpression;
-using code::LessExpression;
-using code::LessOrEqualExpression;
-using code::GreaterExpression;
-using code::GreaterOrEqualExpression;
-using code::AndExpression;
-using code::OrExpression;
-using code::NotExpression;
-using code::InListExpression;
-using code::OnExpression;
-using code::Break;
-using code::Continue;
-using code::Return;
-using code::JumpToEof;
-using code::FunctionCall;
-using code::Block;
-using code::Include;
-using code::For;
-using code::While;
-using code::If;
-using code::Case;
-using code::Switch;
-using code::RuleDefinition;
-using code::Assignment;
-using code::AssignmentOperator;
-using code::ASSIGNMENT_OPERATOR_ASSIGN;
-using code::ASSIGNMENT_OPERATOR_APPEND;
-using code::ASSIGNMENT_OPERATOR_DEFAULT;
-using code::LocalVariableDeclaration;
-using code::ActionsDefinition;
-
-
 namespace ascii = boost::spirit::ascii;
 namespace fusion = boost::fusion;
 namespace qi = boost::spirit::qi;
@@ -102,19 +63,8 @@ private:
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
 template <typename Iterator, typename Skipper>
-struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
+struct HamParser : qi::grammar<Iterator, code::Node*(), Skipper> {
 	HamParser()
 		:
 		HamParser::base_type(fStart),
@@ -159,9 +109,6 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 		using qi::_2;
 		using qi::_3;
 		using qi::_4;
-		using qi::_r1;
-		using qi::_r2;
-		using qi::_r3;
 		using phoenix::begin;
 		using phoenix::construct;
 		using phoenix::end;
@@ -170,9 +117,9 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 		using ascii::char_;
 
 		fAssignmentOperator.add
-			("=", ASSIGNMENT_OPERATOR_ASSIGN)
-			("+=", ASSIGNMENT_OPERATOR_APPEND)
-			("?=", ASSIGNMENT_OPERATOR_DEFAULT)
+			("=", code::ASSIGNMENT_OPERATOR_ASSIGN)
+			("+=", code::ASSIGNMENT_OPERATOR_APPEND)
+			("?=", code::ASSIGNMENT_OPERATOR_DEFAULT)
 		;
 
 		fListDelimiter.add
@@ -252,7 +199,7 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 					[ _val = new_<code::Leaf>(_1) ];
 
 		fList = eps
-				[ _val = new_<List>() ]
+				[ _val = new_<code::List>() ]
 			>> *(fArgument
 					[ *_val += _1 ]
 				);
@@ -261,13 +208,13 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 
 		fAtom
  			= '!' >> fAtom
- 					[ _val = new_<NotExpression>(_1) ]
+ 					[ _val = new_<code::NotExpression>(_1) ]
 			| ('(' >> fExpression >> ')')
 					[ _val = _1 ]
 			| fArgument
 					[ _val = _1 ]
 				>> -("in" >> fList)
-					[ _val = new_<InListExpression>(_val, _1) ]
+					[ _val = new_<code::InListExpression>(_val, _1) ]
 		;
 
 		fExpression
@@ -275,39 +222,40 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 					[ _val = _1 ]
 				>> *(
 					'=' >> fAtom
-						[ _val = new_<EqualExpression>(_val, _1) ]
+						[ _val = new_<code::EqualExpression>(_val, _1) ]
 				|	"!=" >> fAtom
-						[ _val = new_<NotEqualExpression>(_val, _1) ]
+						[ _val = new_<code::NotEqualExpression>(_val, _1) ]
 				|	"<=" >> fAtom
-						[ _val = new_<LessOrEqualExpression>(_val, _1) ]
+						[ _val = new_<code::LessOrEqualExpression>(_val, _1) ]
 				|	'<' >> fAtom
-						[ _val = new_<LessExpression>(_val, _1) ]
+						[ _val = new_<code::LessExpression>(_val, _1) ]
 				|	">=" >> fAtom
-						[ _val = new_<GreaterOrEqualExpression>(_val, _1) ]
+						[ _val = new_<code::GreaterOrEqualExpression>(_val, _1)
+							]
 				|	'>' >> fAtom
-						[ _val = new_<GreaterExpression>(_val, _1) ]
+						[ _val = new_<code::GreaterExpression>(_val, _1) ]
 				|	"&&" >> fAtom
-						[ _val = new_<AndExpression>(_val, _1) ]
+						[ _val = new_<code::AndExpression>(_val, _1) ]
 				|	'&' >> fAtom
-						[ _val = new_<AndExpression>(_val, _1) ]
+						[ _val = new_<code::AndExpression>(_val, _1) ]
 				|	"||" >> fAtom
-						[ _val = new_<OrExpression>(_val, _1) ]
+						[ _val = new_<code::OrExpression>(_val, _1) ]
 				|	"|" >> fAtom
-						[ _val = new_<OrExpression>(_val, _1) ]
+						[ _val = new_<code::OrExpression>(_val, _1) ]
 				)
 		;
 
 		fBracketOnExpression
 			= "on" >> fArgument
-					[ _val = new_<OnExpression>(_1) ]
+					[ _val = new_<code::OnExpression>(_1) ]
 				>> (
 					("return" >> fList)
-						[ bind(&OnExpression::SetExpression, _val, _1) ]
+						[ bind(&code::OnExpression::SetExpression, _val, _1) ]
 				|	fFunctionCall
-						[ bind(&OnExpression::SetExpression, _val, _1) ]
+						[ bind(&code::OnExpression::SetExpression, _val, _1) ]
 				)
 		;
-			// Unfortunately [ _val = new_<OnExpression>(_1, _2) ] doesn't
+			// Unfortunately [ _val = new_<code::OnExpression>(_1, _2) ] doesn't
 			// work here, since the variant isn't converted automatically to
 			// Node*.
 
@@ -318,11 +266,11 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 
 		fFunctionCall
 			= fArgument
-					[ _val = new_<FunctionCall>(_1) ]
+					[ _val = new_<code::FunctionCall>(_1) ]
 				>> -(fList
-						[ bind(&FunctionCall::AddArgument, _val, _1) ]
+						[ bind(&code::FunctionCall::AddArgument, _val, _1) ]
 					>> *(':' >> fList
-							[ bind(&FunctionCall::AddArgument, _val, _1) ]
+							[ bind(&code::FunctionCall::AddArgument, _val, _1) ]
 						)
 					)
 // TODO: Parse fListOfLists here, too.
@@ -332,15 +280,15 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 			= ('{' >> fBlock >> '}')
 					[ _val = _1 ]
 			| ("include" >> fList >> ';')
-					[ _val = new_<Include>(_1) ]
+					[ _val = new_<code::Include>(_1) ]
 			| ("break" >> fList >> ';')
-					[ _val = new_<Break>(_1) ]
+					[ _val = new_<code::Break>(_1) ]
 			| ("continue" >> fList >> ';')
-					[ _val = new_<Continue>(_1) ]
+					[ _val = new_<code::Continue>(_1) ]
 			| ("return" >> fList >> ';')
-					[ _val = new_<Return>(_1) ]
+					[ _val = new_<code::Return>(_1) ]
 			| ("jumptoeof" >> fList >> ';')
-					[ _val = new_<JumpToEof>(_1) ]
+					[ _val = new_<code::JumpToEof>(_1) ]
 			| fIfStatement
 					[ _val = _1 ]
 			| fForStatement
@@ -357,17 +305,17 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 					[ _val = _1 ]
 			| fArgument [_val = _1]
 				>> (("on" >> fList >> fAssignmentOperator >> fList)
-						[ _val = new_<Assignment>(_val, _2, _3, _1) ]
+						[ _val = new_<code::Assignment>(_val, _2, _3, _1) ]
 					| (fAssignmentOperator >> fList)
-						[ _val = new_<Assignment>(_val, _1, _2) ]
+						[ _val = new_<code::Assignment>(_val, _1, _2) ]
 					| fListOfLists
-						[ _val = new_<FunctionCall>(_val, _1) ]
+						[ _val = new_<code::FunctionCall>(_val, _1) ]
 				) >> ';'
 		;
 
 		fBlock
 			= eps
-					[ _val = new_<Block>() ]
+					[ _val = new_<code::Block>() ]
 				>> *(fStatement
 						[ *_val += _1 ]
 					| fLocalVariableDeclaration
@@ -377,9 +325,9 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 
 		fLocalVariableDeclaration
 			= "local" >> fList
-					[ _val = new_<LocalVariableDeclaration>(_1) ]
+					[ _val = new_<code::LocalVariableDeclaration>(_1) ]
 				>> -('=' >> fList
-						[ bind(&LocalVariableDeclaration::SetInitializer,
+						[ bind(&code::LocalVariableDeclaration::SetInitializer,
 							_val, _1) ]
 					)
 				>> ';'
@@ -387,56 +335,57 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 
 		fIfStatement
 			= ("if" >> fExpression >> '{' >> fBlock >> '}')
-					[ _val = new_<If>(_1, _2) ]
+					[ _val = new_<code::If>(_1, _2) ]
 				>> -("else" >> fStatement
-						[ bind(&If::SetElseBlock, _val, _1) ]
+						[ bind(&code::If::SetElseBlock, _val, _1) ]
 					)
 		;
 
 		fForStatement
 			= ("for" >> fArgument >> "in" >> fList >> '{' >> fBlock >> '}')
-					[ _val = new_<For>(_1, _2, _3) ]
+					[ _val = new_<code::For>(_1, _2, _3) ]
 		;
 
 		fWhileStatement
 			= ("while" >> fExpression >> '{' >> fBlock >> '}')
-					[ _val = new_<While>(_1, _2) ]
+					[ _val = new_<code::While>(_1, _2) ]
 		;
 
 		fOnStatement
 			= ("on" >> fArgument >> fStatement)
-					[ _val = new_<OnExpression>(_1, _2) ]
+					[ _val = new_<code::OnExpression>(_1, _2) ]
 		;
 
 		fCaseStatement
 			= ("case" >> fIdentifier >> ':' >> fBlock)
-					[ _val = new_<Case>(_1, _2) ]
+					[ _val = new_<code::Case>(_1, _2) ]
 		;
 
 		fSwitchStatement
 			= ("switch" >> fList)
-					[ _val = new_<Switch>(_1) ]
+					[ _val = new_<code::Switch>(_1) ]
 				>> '{'
 				>> *(fCaseStatement
-						[ bind(&Switch::AddCase, _val, _1) ]
+						[ bind(&code::Switch::AddCase, _val, _1) ]
 					)
 				>> '}'
 		;
 
 		fRuleDefinition
 			= ("rule" >> fIdentifier)
-					[ _val = new_<RuleDefinition>(_1)]
+					[ _val = new_<code::RuleDefinition>(_1)]
 				>> -(fIdentifier
-						[ bind(&RuleDefinition::AddParameterName, _val, _1) ]
+						[ bind(&code::RuleDefinition::AddParameterName, _val,
+							_1) ]
 					>> *(':'
 						>> fIdentifier
-								[ bind(&RuleDefinition::AddParameterName,
+								[ bind(&code::RuleDefinition::AddParameterName,
 									_val, _1) ]
 						)
 					)
 				>> '{'
 				>> fBlock
-						[ bind(&RuleDefinition::SetBlock, _val, _1) ]
+						[ bind(&code::RuleDefinition::SetBlock, _val, _1) ]
 				>> '}'
 		;
 
@@ -445,7 +394,7 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 				>> '{'
 				>> raw[fActions]
 				>> '}')
-						[ _val = new_<ActionsDefinition>(_1, _2, _3,
+						[ _val = new_<code::ActionsDefinition>(_1, _2, _3,
 							construct<std::string>(begin(_4), end(_4))) ]
 		;
 
@@ -501,29 +450,29 @@ struct HamParser : qi::grammar<Iterator, Node*(), Skipper> {
 	}
 
 private:
-	qi::rule<Iterator, Node*(), Skipper> fStart;
-	qi::rule<Iterator, Block*(), Skipper> fBlock;
-	qi::rule<Iterator, LocalVariableDeclaration*(), Skipper>
+	qi::rule<Iterator, code::Node*(), Skipper> fStart;
+	qi::rule<Iterator, code::Block*(), Skipper> fBlock;
+	qi::rule<Iterator, code::LocalVariableDeclaration*(), Skipper>
 		fLocalVariableDeclaration;
-	qi::rule<Iterator, RuleDefinition*(), Skipper> fRuleDefinition;
-	qi::rule<Iterator, ActionsDefinition*(), Skipper> fActionsDefinition;
-	qi::rule<Iterator, If*(), Skipper> fIfStatement;
-	qi::rule<Iterator, For*(), Skipper> fForStatement;
-	qi::rule<Iterator, While*(), Skipper> fWhileStatement;
-	qi::rule<Iterator, Switch*(), Skipper> fSwitchStatement;
-	qi::rule<Iterator, Case*(), Skipper> fCaseStatement;
-	qi::rule<Iterator, Node*(), Skipper> fStatement;
-	qi::rule<Iterator, Node*(), Skipper> fBracketExpression;
-	qi::rule<Iterator, FunctionCall*(), Skipper> fFunctionCall;
-	qi::rule<Iterator, OnExpression*(), Skipper> fBracketOnExpression;
-	qi::rule<Iterator, OnExpression*(), Skipper> fOnStatement;
-	qi::rule<Iterator, Node*(), Skipper> fExpression;
-	qi::rule<Iterator, ListList(), Skipper> fListOfLists;
-	qi::rule<Iterator, List*(), Skipper> fList;
-	qi::rule<Iterator, Node*(), Skipper> fAtom;
-	qi::rule<Iterator, Node*(), Skipper> fArgument;
+	qi::rule<Iterator, code::RuleDefinition*(), Skipper> fRuleDefinition;
+	qi::rule<Iterator, code::ActionsDefinition*(), Skipper> fActionsDefinition;
+	qi::rule<Iterator, code::If*(), Skipper> fIfStatement;
+	qi::rule<Iterator, code::For*(), Skipper> fForStatement;
+	qi::rule<Iterator, code::While*(), Skipper> fWhileStatement;
+	qi::rule<Iterator, code::Switch*(), Skipper> fSwitchStatement;
+	qi::rule<Iterator, code::Case*(), Skipper> fCaseStatement;
+	qi::rule<Iterator, code::Node*(), Skipper> fStatement;
+	qi::rule<Iterator, code::Node*(), Skipper> fBracketExpression;
+	qi::rule<Iterator, code::FunctionCall*(), Skipper> fFunctionCall;
+	qi::rule<Iterator, code::OnExpression*(), Skipper> fBracketOnExpression;
+	qi::rule<Iterator, code::OnExpression*(), Skipper> fOnStatement;
+	qi::rule<Iterator, code::Node*(), Skipper> fExpression;
+	qi::rule<Iterator, code::ListList(), Skipper> fListOfLists;
+	qi::rule<Iterator, code::List*(), Skipper> fList;
+	qi::rule<Iterator, code::Node*(), Skipper> fAtom;
+	qi::rule<Iterator, code::Node*(), Skipper> fArgument;
 	qi::rule<Iterator, uint32_t(), Skipper> fActionsFlags;
-	qi::rule<Iterator, List*(), Skipper> fActionsBindList;
+	qi::rule<Iterator, code::List*(), Skipper> fActionsBindList;
 	qi::rule<Iterator, qi::unused_type> fActions;
 	qi::rule<Iterator, String()> fIdentifier;
 	qi::rule<Iterator, String()> fString;
@@ -533,7 +482,7 @@ private:
 	qi::rule<Iterator, char()> fEscapedChar;
 	qi::symbols<char, unsigned> fListDelimiter;
 	qi::symbols<char, uint32_t> fActionFlag;
-	qi::symbols<char, AssignmentOperator> fAssignmentOperator;
+	qi::symbols<char, code::AssignmentOperator> fAssignmentOperator;
 };
 
 
@@ -605,7 +554,7 @@ const char* kTestString =
 		std::cout << *it;
 	std::cout << "\" ...\n";
 
-	Node* result = NULL;
+	code::Node* result = NULL;
 	bool r = qi::phrase_parse(begin, end,
 		HamParser<IteratorType, HamSkipParser<IteratorType> >(),
 		HamSkipParser<IteratorType>(), result);
@@ -613,7 +562,7 @@ const char* kTestString =
 	if (r && begin == end) {
 		std::cout << "-------------------------\n";
 		std::cout << "Parsing succeeded\n";
-		DumpContext dumpContext;
+		code::DumpContext dumpContext;
 		result->Dump(dumpContext);
 		std::cout << "-------------------------\n";
 	} else {
@@ -623,7 +572,7 @@ const char* kTestString =
 		std::cout << "stopped at: \"" << rest << "\"\n";
 		std::cout << "-------------------------\n";
 if (result != NULL) {
-DumpContext dumpContext;
+code::DumpContext dumpContext;
 result->Dump(dumpContext);
 }
 	}
