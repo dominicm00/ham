70bdc9109de652ed42d778639db3f1bad0875a32
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-11-03 21:30:06 +0100

    Implemented Assignment::Evaluate().

--- src/code/Assignment.cpp
+++ src/code/Assignment.cpp
@@ -8,6 +8,7 @@
 
 #include "code/DumpContext.h"
 #include "code/EvaluationContext.h"
+#include "data/TargetPool.h"
 
 
 using code::Assignment;
@@ -35,8 +36,72 @@ Assignment::~Assignment()
 StringList
 Assignment::Evaluate(EvaluationContext& context)
 {
-	// TODO: Implement!
-	return kFalseStringList;
+	StringList lhs = fLeft->Evaluate(context);
+	StringList rhs = fRight->Evaluate(context);
+
+	if (fOnTargets != NULL) {
+		// Set the variables on the given targets.
+		StringList targets = fOnTargets->Evaluate(context);
+		for (StringList::const_iterator it = targets.begin();
+				it != targets.end(); ++it) {
+			// get the target and its variable domain
+			data::Target* target = context.Targets().LookupOrCreate(*it);
+			data::VariableDomain* domain = target->Variables(true);
+
+			// set the variables
+			for (StringList::const_iterator varIt = lhs.begin();
+					varIt != lhs.end(); ++varIt) {
+				switch (fOperator) {
+					case ASSIGNMENT_OPERATOR_ASSIGN:
+						domain->Set(*varIt, rhs);
+						break;
+					case ASSIGNMENT_OPERATOR_APPEND:
+						std::copy(rhs.begin(), rhs.end(),
+							domain->LookupOrCreate(*varIt).end());
+						break;
+					case ASSIGNMENT_OPERATOR_DEFAULT:
+						if (domain->Lookup(*varIt) == NULL)
+							domain->Set(*varIt, rhs);
+						break;
+				}
+			}
+		}
+	} else {
+		// No targets. If a local variable with the respective name exists, we
+		// set it, otherwise we set one in a global scope.
+		for (StringList::const_iterator it = lhs.begin();
+				it != lhs.end(); ++it) {
+			const String& variable = *it;
+
+			// look for a local variable
+			StringList* data = context.LocalScope()->Lookup(variable);
+			bool created = false;
+			if (data == NULL) {
+				// no local variable -- check for a global one
+				data = context.GlobalScope()->Lookup(variable);
+				if (data == NULL) {
+					// no existing global variable either -- create one
+					data = &context.GlobalVariables()->LookupOrCreate(variable);
+					created = true;
+				}
+			}
+
+			switch (fOperator) {
+				case ASSIGNMENT_OPERATOR_ASSIGN:
+					*data = rhs;
+					break;
+				case ASSIGNMENT_OPERATOR_APPEND:
+					std::copy(rhs.begin(), rhs.end(), data->end());
+					break;
+				case ASSIGNMENT_OPERATOR_DEFAULT:
+					if (created)
+						*data = rhs;
+					break;
+			}
+		}
+	}
+
+	return rhs;
 }
 
 
