6affa50637cab76b409e26607956b40784510b2b
Ingo Weinhold <ingo_weinhold@gmx.de>
2013-02-13 00:04:15 +0100

    code::Include: First implementation
    Still several TODOs.

--- src/code/Include.cpp
+++ src/code/Include.cpp
@@ -6,8 +6,14 @@
 
 #include "code/Include.h"
 
+#include <fstream>
+#include <memory>
+
+#include "code/Block.h"
 #include "code/DumpContext.h"
 #include "code/EvaluationContext.h"
+#include "code/EvaluationException.h"
+#include "parser/Parser.h"
 
 
 namespace ham {
@@ -30,8 +36,31 @@ Include::~Include()
 StringList
 Include::Evaluate(EvaluationContext& context)
 {
-//		StringList fileNames = fFileNames->Evaluate(context);
-// TODO: Include each file. Concatenate the results/return the last one?
+	StringList fileNames = fFileNames->Evaluate(context);
+	if (!fileNames.IsEmpty()) {
+		// Only the file referred to by the first element is included.
+		String fileName = fileNames.ElementAt(0);
+// TODO: Bind target!
+		std::ifstream file(fileName.ToCString());
+		if (file.fail()) {
+			throw EvaluationException(
+				std::string("include: Failed to open file \"")
+					+ fileName.ToCString() + "\"");
+		}
+
+		parser::Parser parser;
+		parser.SetFileName(fileName.ToStlString());
+		std::auto_ptr<code::Block> block(parser.Parse(file));
+// TODO: Once it goes out of scope, it will also take rules defined there down
+// with it!
+
+// TODO: New variable scope?
+		block->Evaluate(context);
+
+		if (context.GetJumpCondition() == JUMP_CONDITION_JUMP_TO_EOF)
+			context.SetJumpCondition(JUMP_CONDITION_NONE);
+	}
+
 	return StringList::False();
 }
 
