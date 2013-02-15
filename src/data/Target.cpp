1dd506d10e66134ebbf0e147dac57e3e8d25358f
Ingo Weinhold <ingo_weinhold@gmx.de>
2013-02-15 20:25:14 +0100

    Target: Add flags

--- src/data/Target.cpp
+++ src/data/Target.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Copyright 2010-2013, Ingo Weinhold, ingo_weinhold@gmx.de.
  * Distributed under the terms of the MIT License.
  */
 
@@ -9,6 +9,24 @@
 
 namespace ham {
 namespace data {
+
+
+Target::Target()
+	:
+	fName(),
+	fVariables(NULL),
+	fFlags(0)
+{
+}
+
+
+Target::Target(const String& name)
+	:
+	fName(name),
+	fVariables(NULL),
+	fFlags(0)
+{
+}
 
 
 Target::~Target()
--- src/data/Target.h
+++ src/data/Target.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Copyright 2010-2013, Ingo Weinhold, ingo_weinhold@gmx.de.
  * Distributed under the terms of the MIT License.
  */
 #ifndef HAM_DATA_TARGET_H
@@ -15,43 +15,44 @@ namespace data {
 
 class Target {
 public:
-	inline						Target();
-	inline						Target(const String& name);
+			enum {
+				BUILD_ALWAYS		= 0x01,
+					// rebuild regardless of date of pre-existing file
+				DONT_UPDATE			= 0x02,
+					// don't update, if file exists, regardless of file date
+				IGNORE_IF_MISSING	= 0x04,
+					// silently ignore the (source) target, if file is missing
+				NOT_A_FILE			= 0x08,
+					// pseudo target
+				TEMPORARY			= 0x10,
+				DEPENDS_ON_LEAVES	= 0x20
+					// target depends on its leaf dependencies only,
+					// intermediate dependencies are ignored
+			};
+
+public:
+								Target();
+								Target(const String& name);
 								~Target();
 
-	inline	void				SetName(const String& name);
+			void				SetName(const String& name)
+									{ fName = name; }
 									// conceptually package private
 
 	inline	VariableDomain*		Variables(bool create);
 
+			uint32_t			Flags() const
+									{ return fFlags; }
+			void				SetFlags(uint32_t flags)
+									{ fFlags = flags; }
+			void				AddFlags(uint32_t flags)
+									{ fFlags |= flags; }
+
 private:
 			String				fName;
 			VariableDomain*		fVariables;
+			uint32_t			fFlags;
 };
-
-
-
-Target::Target()
-	:
-	fName(),
-	fVariables(NULL)
-{
-}
-
-
-Target::Target(const String& name)
-	:
-	fName(name),
-	fVariables(NULL)
-{
-}
-
-
-void
-Target::SetName(const String& name)
-{
-	fName = name;
-}
 
 
 VariableDomain*
