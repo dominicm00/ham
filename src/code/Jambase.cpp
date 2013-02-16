f715ba445b1be6a622ccdccbc1679397b673fa27
Ingo Weinhold <ingo_weinhold@gmx.de>
2013-02-16 09:30:27 +0100

    Add dummy Jambase

---
+++ src/code/Jambase.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2013, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include "code/Jambase.h"
+
+
+namespace ham {
+namespace code {
+
+
+const char* const kJambase =
+#if 0
+#	include "JambaseData.h"
+#else
+	"include Jamfile ;"
+#endif
+	;
+
+
+}	// namespace code
+}	// namespace ham
