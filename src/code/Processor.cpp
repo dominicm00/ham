2fc8885f1a1e1b823b765794a9427d1bc63095be
Ingo Weinhold <ingo_weinhold@gmx.de>
2013-02-16 09:31:47 +0100

    Add code::Processor, a high-level class for using Ham

---
+++ src/code/Processor.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2013, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include "code/Processor.h"
+
+#include "code/Block.h"
+#include "code/BuiltInRules.h"
+#include "code/Jambase.h"
+#include "parser/Parser.h"
+
+
+namespace ham {
+namespace code {
+
+
+Processor::Processor()
+	:
+	fGlobalVariables(),
+	fTargets(),
+	fEvaluationContext(fGlobalVariables, fTargets)
+{
+	code::BuiltInRules::RegisterRules(fEvaluationContext.Rules());
+}
+
+
+Processor::~Processor()
+{
+}
+
+
+void
+Processor::SetCompatibility(behavior::Compatibility compatibility)
+{
+	fEvaluationContext.SetCompatibility(compatibility);
+}
+
+
+void
+Processor::SetBehavior(behavior::Behavior behavior)
+{
+	fEvaluationContext.SetBehavior(behavior);
+}
+
+
+void
+Processor::SetOutput(std::ostream& output)
+{
+	fEvaluationContext.SetOutput(output);
+}
+
+
+void
+Processor::SetErrorOutput(std::ostream& output)
+{
+	fEvaluationContext.SetErrorOutput(output);
+}
+
+
+void
+Processor::SetTargetsToBuild(const StringList& targets)
+{
+	fTargetsToBuild = targets;
+}
+
+
+void
+Processor::ProcessJambase()
+{
+	// parse code
+	parser::Parser parser;
+	parser.SetFileName("Jambase");
+	util::Reference<code::Block> block(parser.Parse(kJambase), true);
+
+	// execute the code
+	block->Evaluate(fEvaluationContext);
+}
+
+
+void
+Processor::BindTargets()
+{
+	// TODO:...
+}
+
+
+void
+Processor::BuildTargets()
+{
+	// TODO:...
+}
+
+
+}	// namespace code
+}	// namespace ham
