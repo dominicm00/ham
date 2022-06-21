76fbb4955f2ef7ded8e98edb36db34db67d05fd3
Dominic Martinez <dom@dominicm.dev>
2022-06-20 21:58:04 -0400

    code: Document Node and NodeVisitor.

--- src/code/Node.h
+++ src/code/Node.h
@@ -22,22 +22,88 @@ class Node;
 using util::Reference;
 using util::Referenceable;
 
+/**
+ * Passed to Node::Visit to recursively visit nodes.
+ */
 class NodeVisitor
 {
   public:
 	virtual ~NodeVisitor();
 
-	// return true to terminate visiting
+    /**
+	 * Called on all Node objects in a subtree.
+	 *
+	 * \param[in] node Node to visit.
+	 * \return whether or not to stop visiting nodes.
+	 */
 	virtual bool VisitNode(Node* node) = 0;
 };
 
+/**
+ * Nodes are the core type for all Ham operations. Ham files are parsed into a
+ * tree of nodes that are then evaluated recursively.
+ */
 class Node : public Referenceable
 {
   public:
 	virtual ~Node();
 
+	/**
+	 * Evaluate the current node. This generally involves evaluating any
+	 * subnodes, then performing the operation the current node represents.
+	 *
+	 * \param[in] context Context to evaluate node under.
+	 *
+	 * \result If the current node is an expression, return the StringList
+	 * associated with the expression result, otherwise return `nullptr`.
+	 */
 	virtual StringList Evaluate(EvaluationContext& context) = 0;
+
+	/**
+	 * Visit nodes recursively until predicate is met.
+	 *
+	 * Nodes should implement this by first checking if
+	 * `visitor.VisitNode(this)` returns true, and returning `this` if it
+	 * does. Otherwise, they should check each subnode and return the first
+	 * match. A simple example is provided below:
+
+	 \code
+     if (visitor.VisitNode(this))
+	   return this;
+
+	 if (Node* result = fLeft->Visit(visitor))
+	   return result;
+
+	 if (Node* result = fRight->Visit(visitor))
+	   return result;
+	 \endcode
+
+	 * \param[in] visitor Contains NodeVisitor::VisitNode predicate.
+	 *
+	 * \result First Node to match predicate.
+	 */
 	virtual Node* Visit(NodeVisitor& visitor) = 0;
+
+	/**
+	 * Dump a string representation of the current node into
+	 * code::DumpContext. Subnodes should be dumped recursively, wrapped in
+	 * calls to DumpContext::BeginChildren and DumpContext::EndChildren. A
+	 * simple example is provided below:
+
+	 \code
+	 context << "BinaryExpressionNode(\n";
+	 context.BeginChildren();
+
+	 fLeft->Dump(context);
+	 fRight->Dump(context);
+
+	 context.EndChildren();
+	 context << ")\n";
+	 \endcode
+
+	 * \param[in] context Dump context for outputting string and denoting
+	 * children.
+	 */
 	virtual void Dump(DumpContext& context) const = 0;
 };
 
