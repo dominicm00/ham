436236a8dd63c78b111d211df0e4c999b67e6181
Dominic Martinez <dom@dominicm.dev>
2022-07-20 18:09:10 -0400

    Add copyright for functionally changed files

--- src/data/RuleActions.hpp
+++ src/data/RuleActions.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2013, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Copyright 2022, Dominic Martinez, dom@dominicm.dev.
  * Distributed under the terms of the MIT License.
  */
 #ifndef HAM_DATA_RULE_ACTIONS_HPP
