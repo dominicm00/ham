82507043aefb175a236262e1a02a739e85c3ff8d
Dominic Martinez <dom@dominicm.dev>
2022-08-24 08:17:10 -0400

    feat(data): add variable scopes (#125)

---
+++ include/public/data/VariableScope.hpp
@@ -0,0 +1,46 @@
+#ifndef HAM_DATA_VARIABLESCOPE_HPP
+#define HAM_DATA_VARIABLESCOPE_HPP
+
+#include "data/Types.hpp"
+
+#include <cstddef>
+#include <functional>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace ham::data
+{
+
+/**
+ * References all accessible variables in a given scope.
+ */
+class VariableScope {
+  public:
+	VariableScope() = default;
+
+	// Copying scopes are expensive; avoid when possible
+	VariableScope(const VariableScope&) = default;
+	VariableScope& operator=(const VariableScope&) = default;
+
+	// May be moved
+	VariableScope(VariableScope&&) = default;
+	VariableScope& operator=(VariableScope&&) = default;
+
+	template<class M>
+	void Set(std::string&& var, M&& list)
+	{
+		var_map.insert_or_assign(std::move(var), std::forward<M>(list));
+	}
+
+	std::optional<std::reference_wrapper<List>> Find(std::string_view var);
+	VariableScope CreateSubscope();
+
+  private:
+	std::optional<std::reference_wrapper<VariableScope>> parent;
+	VariableMap var_map;
+};
+
+} // namespace ham::data
+
+#endif // HAM_DATA_VARIABLESCOPE_HPP
---
+++ src/data/VariableScope.cpp
@@ -0,0 +1,33 @@
+#include "data/VariableScope.hpp"
+
+#include "data/Types.hpp"
+
+#include <functional>
+#include <memory>
+#include <string_view>
+
+namespace ham::data
+{
+
+std::optional<std::reference_wrapper<List>>
+VariableScope::Find(std::string_view var)
+{
+	auto entry = var_map.find(var);
+	if (entry != var_map.end()) {
+		return entry->second;
+	} else if (parent) {
+		return parent.value().get().Find(var);
+	} else {
+		return {};
+	}
+}
+
+VariableScope
+VariableScope::CreateSubscope()
+{
+	VariableScope scope{};
+	scope.parent = *this;
+	return std::move(scope);
+}
+
+} // namespace ham::data
