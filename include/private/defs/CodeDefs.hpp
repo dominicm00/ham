1362ecd669f772273c56fd59be7719e9b8f73e42
Dominic Martinez <dom@dominicm.dev>
2022-08-24 00:24:38 -0400

    feat(code): add generic node utilities
    * Templated base class
    * Testing structure for isolated unit tests
    * Variant helper templates

---
+++ include/private/defs/CodeDefs.hpp
@@ -0,0 +1,26 @@
+#ifndef HAM_DEFS_CODEDEFS_HPP
+#define HAM_DEFS_CODEDEFS_HPP
+
+/**
+ * Ham nodes
+ */
+class Variable;
+class Identifier;
+class Leaf;
+class List;
+
+class ForLoop;
+class IfStatement;
+class WhileLoop;
+
+class ActionDefinition;
+class RuleActionInvocation;
+class RuleDefinition;
+class StatementBlock;
+class VariableAssignment;
+
+class BooleanExpression;
+class LeafComparator;
+class TargetExpression;
+
+#endif // HAM_DEFS_CODEDEFS_HPP
