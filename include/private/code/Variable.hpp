b1b59aaf461895f12f777b010ed84ffe5520fe78
Dominic Martinez <dom@dominicm.dev>
2022-08-24 00:27:54 -0400

    feat(code): add variable

---
+++ include/private/code/Variable.hpp
@@ -0,0 +1,32 @@
+#ifndef HAM_CODE_VARIABLE_HPP
+#define HAM_CODE_VARIABLE_HPP
+
+#include "defs/CodeDefs.hpp"
+
+#include "code/HamNodes.hpp"
+#include "code/Identifier.hpp"
+
+namespace ham::code
+{
+class Variable : public BasicNode<Variable> {
+  public:
+	Variable(AstContext& ast_context, PegtlNode&& pegtl_node);
+
+	data::List Evaluate(EvaluationContext& eval_context) const;
+	std::string String() const;
+	NodeDump Dump() const;
+
+  private:
+	data::List
+	DoSubscript(EvaluationContext& eval_context, const std::string&, const data::List&)
+		const;
+
+  private:
+	std::string content;
+	std::unique_ptr<Node> id;
+	std::unique_ptr<Node> subscript;
+};
+
+} // namespace ham::code
+
+#endif // HAM_CODE_VARIABLE_HPP
